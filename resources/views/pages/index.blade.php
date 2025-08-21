931d3260364884d2363c9d7d4cbfb45e2926c223
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-08-21 23:30:33 +0100

    🎨 Add app branding

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -20,7 +20,13 @@
         @endif
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
+        <header class="flex justify-between items-center w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
+            <h1 class="text-3xl leading-[3.5rem] font-bold">
+                <a href="{{ url('/') }}" class="inline-flex items-center text-blue-500">
+                <img src="{{ asset('logo.svg') }}" alt="{{ config('app.name') }} Logo" class="w-8 h-8 inline-block mr-2" />
+                {{ config('app.name') }}
+                </a>
+            </h1>
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
                     @auth
