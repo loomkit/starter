579a830d5d05ac9f042cbb642846789b0b9a7698
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-08-21 22:53:21 +0100

    🧑‍💻 Use TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
+      input: ['resources/css/app.css', 'resources/ts/app.ts'],
       refresh: true,
     }),
     tailwindcss(),
