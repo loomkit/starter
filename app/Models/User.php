9089c4cc9ea467581e83f8ca9f370f7302de6ae8
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-08-21 21:59:33 +0100

    🔒️ Implement Filament User

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\AdminRole;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasPermissions;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
@@ -50,5 +53,14 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if ($panel->getId() === config('panels.admin.id')) {
+            return $this->hasAnyRole(AdminRole::values()) || auth()->guard(config('panels.admin.guard'))->check();
+        }
+
+        return $panel->getId() === config('panels.app.id') && auth()->guard(config('panels.app.guard'))->check();
     }
 }
